c4b6c80b9f9fb595754a39c4d723add81d5eddd2
danimaeztu <daniel.maeztu@gmail.com>
2026-05-01 01:28:19 +0200

    1.0

---
+++ Source/config.py
@@ -0,0 +1,12 @@
+# TODO: Fill the gaps
+
+# dynu API Auth
+dynu_user = ''
+dynu_pass = ''
+
+# MySQL user and password
+sql_user = ''
+sql_pw = ''
+
+# Templates
+templates_path = ''
--- config.py
+++
@@ -1,9 +0,0 @@
-# TODO: Fill the gaps
-
-# dynu API Auth
-dynu_user = ''
-dynu_pass = ''
-
-# MySQL user and password
-sql_user = ''
-sql_pw = ''
